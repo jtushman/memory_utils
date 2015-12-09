5ed66173fdddb6de8df92e2eb252e97b83b23350
Jonathan Tushman <jtushman@insightsquared.com>
2015-12-09 16:34:09 -0500

    support older version of colorrama

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 packages = ['memory_utils']
 required_modules = [
-    "colorama>=0.3.2",
+    "colorama>=0.2.0",
     "psutil>=3.0.0",
     "six>=1.8.0"
 ]
 
 
 setup(name='memory_utils',
-      version='1.0.0',
+      version='1.0.1',
       description='Tools to help with memory leaks',
       url='http://github.com/jtushman/memory_utils',
       author='Jonathan Tushman',
